e44f05487c145b73afc6bd674552e32a6e8a7eac
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-08 21:58:38 +0200

    added Core

--- core.js
+++ core.js
@@ -1,5 +1,10 @@
-var names= "Emmanuel"
-if (names == "Emmanuel")
-{
-console.log(" Hello " + names)
-}
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://example.com/api/data');
+xhr.send();
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const response = JSON.parse(xhr.responseText);
+    console.log(response);
+  }
+};
