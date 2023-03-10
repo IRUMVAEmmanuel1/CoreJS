21053f73ee92de86f6525445191d4beeff7f78dc
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-10 20:42:01 +0200

    Nested Loops

--- NestedForLoos.js
+++ NestedForLoos.js
@@ -12,3 +12,10 @@ for (var i = 100; i > 10; i = i - 10) {
         console.log(i + " divided by " + j + " equals " + i / j);
     }
 }
+
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < cubes.length; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
