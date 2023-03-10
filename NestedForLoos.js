af313fb92f18e1b984e8adbe4de6cffd1f44b4a2
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-10 20:38:44 +0200

    Nested Loops

--- NestedForLoos.js
+++ NestedForLoos.js
@@ -7,4 +7,6 @@ for (var year = 2023; year<2025;year++){
 
 //nested loops - one inside another 
 for (var i = 100; i > 10; i = i - 10) {
-    
+    for (var j = 10; j > 4; j = j - 5) {
+       
+        
