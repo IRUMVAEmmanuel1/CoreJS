ab6577abaca8527013b4ee0755726eb6559655e3
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-12 22:47:29 +0200

    Create arrays and access contents

--- Array.js
+++ Array.js
@@ -1,6 +1,2 @@
-numbers= [1,2,3,4,5,6,7,8,9,10];
-for(var i = 0; i.length; i++)
-{
-    numbers.sort()
-    console(numbers)
-}
+var train1 = ["coco", "Isimbi","kelia","clarisse"]
+console.log(train1[3])
