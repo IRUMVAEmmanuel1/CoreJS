61942fa4809952a0da3b143aabf012f40979e9bd
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-12 23:03:28 +0200

    Create arrays and access contents Trst

--- TestArrays.js
+++ TestArrays.js
@@ -1,5 +1,8 @@
 function listArraysItems(arr){
     for(var i=0; i<arr.length; i++){
-
+        console.log(i, arr[i])
     }
 }
+
+var colors = ["red","orange","yellow","green","blue","purple","pink"]
+listArraysItems(colors)
