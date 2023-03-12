37ee7cfe2e2c589704ca3300a79220b70d7efc67
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-12 23:38:04 +0200

    Create arrays and access contents Test

--- functionsTest.js
+++ functionsTest.js
@@ -1,5 +1,14 @@
-letterFinder(word,match){
+letterFinder(word,match)
+{
+
+
     for(var i; i<word.length; i++){
-        
+       if(word[i] == match){
+        console.log('Found the match',match,"at",i)
+       } 
+       else{
+        console.log('----No Match found at',i)
+       }
     }
 }
+letterFinder("test","t")
