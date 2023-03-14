bc05526256c3f98afe40d07c21a4d86fe02a7e59
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-14 19:00:59 +0200

    Object Core Added

--- objects.js
+++ objects.js
@@ -6,3 +6,10 @@ var assistantManager = {
     specialAbility: "Young and ambitions",
     greetings: "Let's make some money"
 }
+
+var table = {
+    legs: 3,
+    color: "brown",
+    priceUSD: 100,
+}
+console.log(table);//display the object in the developer console
