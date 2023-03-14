7b2c718d685dd193f7f299ef6bec0f222dd4c644
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-14 18:59:32 +0200

    Object Core Added

--- objects.js
+++ objects.js
@@ -1,1 +1,8 @@
-var user = {}; //create an object
+var assistantManager = {
+    rangeTitesPerTurn:3,
+    socialSkills:30,
+    streetSmats:30,
+    health:40,
+    specialAbility: "Young and ambitions",
+    greetings: "Let's make some money"
+}
