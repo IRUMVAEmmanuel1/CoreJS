cc85bededdc417e29dfd3224b6deabcf96ba2292
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-14 19:05:52 +0200

    Object Core Added

--- objects.js
+++ objects.js
@@ -13,3 +13,17 @@ var table = {
     priceUSD: 100,
 }
 console.log(table);//display the object in the developer console
+
+var house = {
+    rooms: 3,
+    color: "brown",
+    priceUSD: 10000,
+}
+var house2 = {};
+house2.rooms = 4;
+house2.color = "pink";
+house2.priceUSD = 12345;
+house2.windows = 20;
+console.log(house2); // {rooms: 3, color: "brown", priceUSD: 10000}
+house.windows = 10;
+console.log(house); // {rooms: 3, color: "brown", priceUSD: 10000, windows: 10}
