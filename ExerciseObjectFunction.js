676dc900913ded51180a8e22edfe8bef92b887ab
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-15 20:52:47 +0200

    String chear Sheet

---
+++ ExerciseObjectFunction.js
@@ -0,0 +1,16 @@
+var clothers = [];
+clothers.push("Jeans")
+clothers.push("T-Shirt")
+clothers.push("Jumper")
+clothers.push("valeze")
+clothers.push("short")
+
+clothers.pop();
+clothers.push("shirt")
+console.log(clothers[2]);
+
+var favcar = [];
+favcar.color="red"
+favcar.covertible = true
+
+console.log(favcar)
