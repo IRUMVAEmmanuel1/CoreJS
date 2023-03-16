3e4bd7496cb1866589eeef217b6116dffb379786
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-16 11:58:28 +0200

    Error Handling

---
+++ ErrorHandling.js
@@ -0,0 +1,6 @@
+try {
+    a=12;b=12
+    console.log(a+b)
+} catch (err) {
+    console.log("there was an error")
+}
---
+++ ObjectMethods.js
@@ -0,0 +1,15 @@
+var car ={};
+car.color="red";
+car.tutrnKey = function(){
+    console.log("Engine running");
+}
+
+car.lightOn = function(){
+    console.log("the light are on")
+
+}
+
+console.log(car)
+
+car.tutrnKey();
+car.lightOn();
