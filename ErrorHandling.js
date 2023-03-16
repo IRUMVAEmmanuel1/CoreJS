fe3488d15323d59a2e06883de9aca3a115d9a8d2
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-16 12:27:16 +0200

    Error Handling

--- ErrorHandling.js
+++ ErrorHandling.js
@@ -5,3 +5,11 @@ try {
     console.log("there was an error")
 }
 console.log("My program does not stop")
+
+
+// Types of error
+// TypeError
+// ReferenceError
+// RangeError
+// SyntaxError
+// undefined
