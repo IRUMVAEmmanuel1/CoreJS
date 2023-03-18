6bee01d7492377d17f7467b088b6540cc7565d93
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-18 23:11:35 +0200

    Function callling and recursion

--- FunctionCalling&Recursion.js
+++ FunctionCalling&Recursion.js
@@ -4,3 +4,14 @@ function example(){
     console.log("Line Three");
 }
 example()
+
+
+let counter =3 
+function example(){
+    console.log(counter);
+    counter =counter-1;
+    if(counter===0){
+        return;
+        example();
+    }
+}
