91efdec4aeb4280fda030a35b684ecd0600b7d78
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-20 22:08:10 +0200

    scoping with var,const and let

--- exercises.js
+++ exercises.js
@@ -1,5 +1,16 @@
-function vowels(myName){
-    vowels = myName.match(/[aeiou]/gi).length
-    console.log(vowels)
+// function vowels(myName){
+//     vowels = myName.match(/[aeiou]/gi).length
+//     console.log(vowels)
+// }
+// vowels("irumvaemmanuel")
+
+
+var shoes = 100;
+var tax = 1.2;
+function totalprice(){
+    return shoes*tax;
+
 }
-vowels("irumvaemmanuel")
+
+var totalpay = totalprice(shoes,tax);
+console.log(totalpay)
