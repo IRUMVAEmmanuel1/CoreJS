3c3f4ad3f32d267068e6d0ce00a866b8416367c5
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-20 23:29:24 +0200

    Class

--- class.js
+++ class.js
@@ -8,3 +8,5 @@ console.log (Animal)
 class Animal { /* ...class code here... */ }
 class Bird extends Animal { /* ...class code here... */ }
 class Eagle extends Bird { /* ...class code here... */ }
+
+new Date();
