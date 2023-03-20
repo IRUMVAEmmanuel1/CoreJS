87836223350b3cc89cc71aa5e6af41058309a39c
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-20 22:57:50 +0200

    Class

---
+++ class.js
@@ -0,0 +1,10 @@
+class Animal { /* ...class code here... */ }
+
+var myDog = Object.create(Animal)
+
+console.log (Animal)
+
+
+class Animal { /* ...class code here... */ }
+class Bird extends Animal { /* ...class code here... */ }
+class Eagle extends Bird { /* ...class code here... */ }
