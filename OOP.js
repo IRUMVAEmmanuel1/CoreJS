8487eb396e4b980ec27e98921e2f2a1cd8208c5f
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-21 15:18:21 +0200

    Corrected Item with prices after discount

--- OOP.js
+++ OOP.js
@@ -1,29 +1,38 @@
-var items = [
-  {
-    itemName: "Banana",
-    itemPrice: 100,
-    tax: 1.2,
-  },
-  {
-    itemName: "Mango",
-    itemPrice: 100,
-    discount: 1.2,
-  },
-  {
-    itemName: "Banana",
-    itemPrice: 100,
-    discount: 1.2,
-  },
-  {
-    itemName: "Banana",
-    itemPrice: 100,
-    discount: 1.2,
-  },
-];
+let arrayitem = [
+    {
+        itemName: "Banana",
+        ItemPrice: 2000,
+        discount: 2.3,
+    },
+    {
+        itemName: "Pizza",
+        ItemPrice: 8000,
+        discount: 1.5,
+    },
+    {
+        itemName: "Banana",
+        ItemPrice: 2000,
+        discount: 2.3,
+    },
+    {
+        itemName: "Mango",
+        ItemPrice: 2200,
+        discount: 2.3,
+    },
+    {
+        itemName: "Biscuit vanilla",
+        ItemPrice: 2000,
+        discount: 2.3,
+    },
+    {
+        itemName: "Yoghut",
+        ItemPrice: 1000,
+        discount: 2.3,
+    },
+]
 
-let array = [];
+let array =[];
 
-for (let item of items) {
-  array.push({ ...item, totaPrice: item.itemPrice * item.discount });
+for( let item in arrayitem){
+    
 }
-console.log(array);
