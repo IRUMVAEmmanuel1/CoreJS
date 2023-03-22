40d5f80ed280fb7973ac863480ca734b79008fd2
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-22 23:01:55 +0200

    Swapped Array

--- arrayInObject.js
+++ arrayInObject.js
@@ -1,25 +1,6 @@
-var fruits=[];
-
 // inserting item in array
 fruits.push("Apple");
 fruits.push("pear")
 // console.log(fruits)
 
-
-
-// removing last option
-fruits.pop();
-// console.log(fruits)
-
-// functions that takees all its arguments and pushes them into an array
-
-function arrayInObject(one,two,three){
-    var arr=[];
-    arr.push(one);
-    arr.push(two)
-    arr.push(three)
-    return arr
-} 
-var simplearr = arrayInObject("apple","pear","banana")
-console.log(simplearr);
 
