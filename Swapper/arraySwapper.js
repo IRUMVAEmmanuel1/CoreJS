d10e58777e233ffa42ffce13ab5a4457d732741c
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-22 23:07:56 +0200

    this is swapper

---
+++ Swapper/arraySwapper.js
@@ -0,0 +1,20 @@
+// inserting item in array
+fruits.push("Apple");
+fruits.push("pear")
+// console.log(fruits)
+
+
+// removing last option
+fruits.pop();
+// console.log(fruits)
+
+// functions that takees all its arguments and pushes them into an array
+
+function arrayInObject(one,two,three){
+    var arr=[];
+    arr.push(one);
+    arr.push(two)
+    arr.push(three)
+    return arr
+} 
+var simplearr = arrayInObject("apple","pear","banana")
