eedd216f489b695c7b1b29140190f0f2fabbbe5f
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-23 17:13:43 +0200

    class with inheritance

--- InheritancewithClass.js
+++ InheritancewithClass.js
@@ -2,8 +2,12 @@ class train {
     constructor(color, lightOn){
         this.color = color;
         this.lightOn = lightOn;
-        
+
 
     }
 
 }
+
+new train()
+var myFirstTrain = new train('red', false)
+console.log(myFirstTrain)
