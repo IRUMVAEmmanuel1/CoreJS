4d5be81a2dbdd11e895c2f78dd4e2f86ee4c8d39
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-23 17:42:01 +0200

    Default parameters with classes

--- DefaultParameters.js
+++ DefaultParameters.js
@@ -1,3 +1,16 @@
-function  withDefaultParams(number =10){
-    console.log('Result:', number * number)
+class NoDefaultParams {
+  constructor(num1, num2, num3, string1, bool1) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.num3 = num3;
+    this.string1 = string1;
+    this.bool1 = bool1;
+  }
+  calculate(){
+    if(this.bool1){
+        console.log(this.string1, this.num1 + this.num2 + this.num3);
+        return;
+    }
+    return "The value of bool1 is incorrect"
+  }
 }
