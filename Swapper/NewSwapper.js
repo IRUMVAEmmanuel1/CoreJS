98978541ecb9b8f75084cd8d0e8e244021a07185
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-23 13:24:27 +0200

    Swapper

--- Swapper/NewSwapper.js
+++ Swapper/NewSwapper.js
@@ -1,11 +1,9 @@
-function swapper(arr, index1, index2){
+function swapper(arr, index1, index2) {
     let hold = arr[index1];
-
-    arr[index2] = arr[index1]
-
+    arr[index1] = arr[index2]
     arr[index2] = hold;
-    return arr
-
+    return arr;
 }
-let arr=[1,2,3,4]
-console.log(swapper(arr,1,2))
+
+let arr = [5,10,15,20]
+console.log(swapper(arr,0,2))
