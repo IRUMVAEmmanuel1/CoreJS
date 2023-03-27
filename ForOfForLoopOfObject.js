a3b897186bbcb63db04ba7e223ae008f564fdd47
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-27 17:51:47 +0200

    for of objects and array

--- ForOfForLoopOfObject.js
+++ ForOfForLoopOfObject.js
@@ -1,4 +1,11 @@
 const car = ["red","green","blue"]
 for(var prop of car){
-    console.log(prop)
+    // console.log(prop)
 }
+
+
+const car2 = {
+    speed:300,
+    color: "red"
+}
+console.log(Object.keys(car2))
