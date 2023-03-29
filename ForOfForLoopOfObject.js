36c8be990013c4576d6a3edbbbc85003a8338ee0
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-29 16:54:12 +0200

    self tasks

--- ForOfForLoopOfObject.js
+++ ForOfForLoopOfObject.js
@@ -8,4 +8,16 @@ const car2 = {
     speed:300,
     color: "red"
 }
-console.log(Object.keys(car2))
+// console.log(Object.keys(car2))
+
+const car3 = {
+    speed:300,
+    color: "red"
+}
+console.log(Object.values(car2))
+
+const car4 = {
+    speed:300,
+    color: "red"
+}
+console.log(Object.values(car2))
---
+++ self.js
@@ -0,0 +1,35 @@
+function myvowels(str) {
+	var myname = str.match(/[aeiou]/gi).length;
+	console.log("Vowels which is in ", str, "is", myname);
+}
+
+myvowels("mukamuroreunkwere");
+
+// Q2. name of product
+
+var cart = [
+	{
+		pName: "banana",
+		price: 2000,
+		shop: simba,
+		discount: 2.3,
+	},
+	{
+		pName: "orange",
+		price: 1000,
+		shop: simba,
+		discount: 2.3,
+	},
+	{
+		pName: "mango",
+		price: 100,
+		shop: simba,
+		discount: 2.3,
+	},
+	{
+		pName: "Apple",
+		price: 200,
+		shop: simba,
+		discount: 2.3,
+	},
+];
