b1016456f3c73aac52ff53a87703268f7e9cc9ef
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-03-30 18:23:31 +0200

    Data Structures with array, objects and Map

--- DataStructurre.js
+++ DataStructurre.js
@@ -1,2 +1,9 @@
 // working with array in JS
 
+// forEacch Method
+
+const fruits = ["kiwi", "mango", "apple", "pear"];
+function appendIndex(fruit, index) {
+	console.log(`${index} ${fruit}`);
+}
+appendIndex();
