5c9159e286c2fae1b0d39f63595b3ad0ed41bbf9
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-04-03 23:04:27 +0200

    Error Handling

--- ErrorPrevention.js
+++ ErrorPrevention.js
@@ -1,20 +1,5 @@
 
-function addTwoNums(a,b){
-    try {
-        if(typeof(a)!= "number"){
-            console.log("the first argument is not a number")
-        }
-        else if(typeof(b) != "number"){
-            throw new ReferenceError("b is not a number")
-        }
-        else{console.log(a+b)}
-    
-        
-       } catch (err) {
-        console.log("Can't calculate Sum")
-       }   
- 
-}
+
 
 addTwoNums(5,"5")
 
