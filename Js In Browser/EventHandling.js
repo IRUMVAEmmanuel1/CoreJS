b486e0d753504e4aec934f00b588bc599afb2ef9
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-04-04 21:15:59 +0200

    web pages content updates

---
+++ Js In Browser/EventHandling.js
@@ -0,0 +1,11 @@
+const target = document.querySelector('body')
+
+function handleClick(){
+    console.log("Body Clicked")
+}
+
+target.addEventListener('click', handleClick)
+
+function handleClick2(){
+    console.log('clicked heading')
+}
